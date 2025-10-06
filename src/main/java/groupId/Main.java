efef1dc6e8b0c9408a7d483d46d9c24ec1c5c29c
Sergey Nazarov <ric19902002@icloud.com>
2025-10-06 08:35:11 +0000

    Update Main.java

--- src/main/java/groupId/Main.java
+++ src/main/java/groupId/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/skillboxx";
         String user = "root";
-        String password = "Ric137!Marina!";
+        String password = "1234567";
         Connection connection = DriverManager.getConnection(url, user, password);
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT MONTH(subscription_date) AS month, COUNT(*) AS total_courses," +
